362a5958050c9bf7d3e4cdaa210a2dc8c57667c0
denis <1@1.1>
2024-04-02 20:46:00 +0300

    изменение пайплайна 2

--- obj/Debug/net6.0/Hotels.AssemblyInfo.cs
+++ obj/Debug/net6.0/Hotels.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hotels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d088f027d6c79c7804358403a08c3e208d548589")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c39c119f24d2897316d9c8518cb414de25f17463")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hotels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hotels")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
